7ff9dd3231e4762c1ba70f684e5cf2d6e6c9a280
githinkgp <githinkgp@gmail.com>
2019-04-17 08:26:47 -0400

    /NMPC

--- move.py
+++ move.py
@@ -6,9 +6,9 @@ from array import *
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 
-def move(x,u,w,dt):
-    x_new=x[0]+dt*u*math.cos(x[2])
-    y_new=x[1]+dt*u*math.sin(x[2])
-    t_new=x[2]+dt*w
+def move(x,u,dt):
+    x_new=x[0]+dt*u[0]*math.cos(x[2])
+    y_new=x[1]+dt*u[0]*math.sin(x[2])
+    t_new=x[2]+dt*u[1]
     
     return x_new, y_new, t_new
